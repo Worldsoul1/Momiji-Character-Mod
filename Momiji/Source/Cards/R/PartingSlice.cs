e907f076f5e26e7517ce85f94959acac7ecc6522
Worldsoul15 <Worldsoul895@gmail.com>
2024-09-29 20:54:44 -0400

    Added a new card: Parting Slice, and reworked Lookout's Intuition.

--- Momiji/Source/Cards/R/PartingSlice.cs
+++ Momiji/Source/Cards/R/PartingSlice.cs
@@ -1,9 +1,10 @@
-using LBoL.Base;
+﻿using LBoL.Base;
 using LBoL.ConfigData;
 using LBoLEntitySideloader.Attributes;
 using System.Collections.Generic;
 using Momiji.Source;
 using Momiji.Source.GunName;
+using Momiji.Source.StatusEffects;
 using LBoL.Core.Battle;
 using LBoL.Core;
 using LBoL.Core.StatusEffects;
@@ -12,32 +13,32 @@ using System.Linq;
 using LBoL.Core.Battle.Interactions;
 using Momiji.Source.BattleActions;
 using LBoL.EntityLib.StatusEffects.Basic;
-using LBoL.Core.Battle.BattleActions;
 using LBoL.Core.Units;
+using LBoL.Core.Battle.BattleActions;
 
 namespace Momiji.Source.Cards
 {
-    public sealed class LookoutsIntuitionDef : SampleCharacterCardTemplate
+    public sealed class PartingSliceDef : SampleCharacterCardTemplate
     {
         public override CardConfig MakeConfig()
         {
             CardConfig config = GetCardDefaultConfig();
 
-            config.Colors = new List<ManaColor>() { ManaColor.Red };
-            config.Cost = new ManaGroup() { Any = 2, Red = 1 };
-            config.UpgradedCost = new ManaGroup() { Any = 1, Red = 1 };
-            config.Rarity = Rarity.Common;
+            config.Colors = new List<ManaColor>() { ManaColor.White, ManaColor.Red };
+            config.Cost = new ManaGroup() { Any = 1,White = 1, Red = 1 };
+            config.Rarity = Rarity.Rare;
 
-            config.Type = CardType.Skill;
+            config.Type = CardType.Attack;
             config.TargetType = TargetType.AllEnemies;
 
-            config.Value1 = 2;
+            config.Damage = 18;
+            config.UpgradedDamage = 12;
 
-            config.Value2 = 10;
-            config.UpgradedValue2 = 10;
+            config.Value1 = 1;
+            config.Value2 = 2;
 
-            config.RelativeEffects = new List<string>() { nameof(Vulnerable), nameof(Graze) };
-            config.UpgradedRelativeEffects = new List<string>() { nameof(Vulnerable), nameof(Graze) };
+            config.RelativeEffects = new List<string>() { nameof(Graze), nameof(OffensiveIntention) };
+            config.UpgradedRelativeEffects = new List<string>() { nameof(Graze) };
 
             config.Illustrator = "";
 
@@ -45,17 +46,14 @@ namespace Momiji.Source.Cards
             return config;
         }
     }
-    
-    [EntityLogic(typeof(LookoutsIntuitionDef))]
-    public sealed class LookoutsIntuition : SampleCharacterCard
-    {
 
+    [EntityLogic(typeof(PartingSliceDef))]
+    public sealed class PartingSlice : SampleCharacterCard
+    {
         protected override IEnumerable<BattleAction> Actions(UnitSelector selector, ManaGroup consumingMana, Interaction precondition)
         {
             int intention = 0;
             int count = 0;
-            EnemyUnit selectedEnemy = selector.SelectedEnemy;
-            yield return base.BuffAction<Graze>(base.Value1, 0, 0, 0, 0.2f);
             EnemyUnit[] enemies = selector.GetEnemies(base.Battle);
             foreach (EnemyUnit enemy in enemies)
             {
@@ -63,13 +61,20 @@ namespace Momiji.Source.Cards
                 if (intention == 1 || intention == 3 || intention == 5)
                     count++;
             }
-            if (count > 0)
+            yield return base.AttackAction(selector, base.GunName);
+            if (this.IsUpgraded)
             {
-                yield return new ApplyStatusEffectAction<Vulnerable>(selectedEnemy, base.Value1, 0, 0, 0, 0.2f);
+                yield return base.AttackAction(selector, base.GunName);
+            }
+            if (base.Battle.BattleShouldEnd)
+            {
+                yield break;
+            }
+            if(count > 0)
+            {
+                yield return new ApplyStatusEffectAction<Graze>(base.Battle.Player, base.Value1 * count, 0, 0, 0, 0.2f);
             }
             yield break;
         }
     }
 }
-
-
